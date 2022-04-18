9a465172a17eed658150c8342c834bfac94c2024
Leonardo Gonçalves <leonard296@live.com>
2022-04-18 18:48:38 -0300

    create nav bar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,43 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+
+      <div class="container">
+
+        <nav class="row">
+          <div class="col-12">
+            <div class="flex justify-end q-col-gutter-sm">
+
+              <q-input dense outlined v-model="text" label="Search">
+                <template v-slot:append>
+                  <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
+                  <q-icon name="search" class="cursor-pointer" />
+                </template>
+              </q-input>
+
+              <q-select item-aligned dense outlined v-model="sortSelected" :options="['Mais recente', 'Menos recente']" />
+            </div>
+          </div>
+        </nav>
+
+      </div>
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      text: '',
+      sortSelected: 'Sort'
+    }
+  }
+}
 </script>
+
+<style lang="sass">
+.container
+  max-width: 960px
+  margin: 3rem auto 0
+</style>
