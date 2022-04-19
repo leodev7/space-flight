719b2d46b9899d2b58acd7b4012dcfc5122f59ef
Leonardo Gonçalves <leonard296@live.com>
2022-04-19 14:44:34 -0300

    change content to table

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,11 @@
 <template>
   <q-page>
 
-      <div class="container">
+    <div class="container">
 
-        <nav class="row">
+      <q-table class="no-shadow no-border" hide-pagination row-key="id" :rows="articlesData" :columns="articlesColumns" :rows-per-page-options="[10]" @request="onLoadCurrentFilter" >
+
+        <template v-slot:top>
           <div class="col-12">
             <div class="flex justify-end q-col-gutter-sm">
 
@@ -17,46 +19,54 @@
               <q-select item-aligned dense outlined v-model="sortSelected" :options="['Mais recente', 'Menos recente']" />
             </div>
           </div>
-        </nav>
 
-        <section class="row">
           <div class="col-12">
-            <div style="display: flex; flex-direction: column; align-items: center;">
-
+            <div class="flex column flex-center">
               <q-img src="img/logo-nasa.jpg" width="200px" />
               <h5 class="q-my-none">Space Flight News</h5>
-
             </div>
+
+            <q-separator color="grey" size="2px" class="q-mt-xl" />
           </div>
-        </section>
+        </template>
 
-        <q-separator color="grey" size="2px" class="q-my-xl" />
-
-        <section class="row justify-center q-my-xl q-py-lg" v-for="(article, index) in articles" :key="article.id">
-          <div class="col-8">
-
-            <div class="flex q-col-gutter-xl" :style="index % 2 === 0 ? 'flex-direction: row' : 'flex-direction: row-reverse'">
-              <div class="col" :style="index % 2 === 0 ? '' : 'text-align: right'">
-                <q-img class="rounded-borders" :src="article.imageUrl" :ratio="1" />
+        <template v-slot:body="props">
+          <q-tr class="row justify-center q-my-xl q-pa-lg" :props="props" :style="props.rowIndex % 2 === 0 ? 'background: #FFEBEE' : ''">
+            <div class="flex q-col-gutter-xl" :style="props.rowIndex % 2 === 0 ? 'flex-direction: row' : 'flex-direction: row-reverse'">
+              <div class="col" :style="props.rowIndex % 2 === 0 ? '' : 'text-align: right'">
+                <q-img class="rounded-borders" :src="props.row.imageUrl" :ratio="4/3">
+                  <template v-slot:error>
+                    <div class="absolute-full flex flex-center bg-negative text-white">
+                      Erro no servidor da imagem
+                    </div>
+                  </template>
+                </q-img>
               </div>
 
               <div class="col">
-                <p style="font-size: 1rem; font-weight: bold; margin: 0">{{ article.title }}</p>
+                <p style="font-size: 1rem; font-weight: bold; margin: 0">{{ props.row.title }}</p>
                 <div class="flex justify-between">
-                  <p>{{ dateFormated(article.publishedAt) }}</p>
-                  <q-chip dense square color="deep-orange" text-color="white" style="margin-top: 0" :label="article.newsSite" />
+                  <p>{{ dateFormated(props.row.publishedAt) }}</p>
+                  <q-chip dense square color="deep-orange" text-color="white" style="margin-top: 0" :label="props.row.newsSite" />
                 </div>
-                <p style="letter-spacing: 0.1em;">{{ article.summary }}</p>
+                <p style="letter-spacing: 0.1em;">{{ props.row.summary }}</p>
                 <q-btn color="primary" label="Ver mais" />
               </div>
             </div>
+          </q-tr>
+        </template>
 
-            <q-separator style="background: #D07017" size="2px" class="q-mt-xl" />
-
+        <template v-slot:no-data>
+          <div class="row full-width">
+            <div class="q-mt-xs q-px-xs col-12">
+              <p>Nenhum registro encontrado</p>
+            </div>
           </div>
-        </section>
+        </template>
 
-      </div>
+      </q-table>
+
+    </div>
 
   </q-page>
 </template>
@@ -71,7 +81,8 @@ export default {
       text: '',
       sortSelected: 'Sort',
 
-      articles: []
+      articlesColumns:[],
+      articlesData: []
     }
   },
 
@@ -81,10 +92,10 @@ export default {
 
   methods: {
 
-    onLoadCurrentFilter ()  {
+    onLoadCurrentFilter () {
       this.$axios({ method: 'get', url: 'https://api.spaceflightnewsapi.net/v3/articles' })
         .then((response) => {
-          this.articles = response.data
+          this.articlesData = response.data
         })
         .catch((err) => {
           console.log(err)
